ed9e13826a44cc0ea4478d297f648cef187a3d61
muroa96 <thsgskwhd@gmail.com>
2023-12-21 13:33:35 +0900

    Refactor: id of body
    id body to main

--- _main/static/js/dashboard.js
+++ _main/static/js/dashboard.js
@@ -1,18 +1,17 @@
 import { NavManager } from "../../../static/js/core/component/nav-manager";
-
+import { AppDashboard } from "../../../static/js/pages/main/dashboard/app-dashboard";
 export function dashboard_run(context) {
   const parsedContext = JSON.parse(context);
-  const { demo, usertype, studentId, userLogin } = parsedContext;
+  const { usertype, studentId, userLogin } = parsedContext;
 
   if (!userLogin) {
     window.location.href = "../user/";
   }
 
-  const main = document.createElement("main");
-  main.setAttribute("id", "main");
+  const main = document.querySelector("#main");
 
   const clNav = new NavManager(parsedContext);
-  const clManager = new DashboardApp({ demo, usertype, studentId, userLogin });
+  const clManager = new AppDashboard({ usertype, studentId, userLogin });
 
   main.append(clNav.getElement(), clManager.getElement());
 }
