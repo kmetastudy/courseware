96b9d6117315ab47bf1745eea61e3f978bd7d60a
muroa96 <thsgskwhd@gmail.com>
2023-12-26 10:40:11 +0900

    Fix: add csrftoken

--- _main/static/js/dashboard.js
+++ _main/static/js/dashboard.js
@@ -4,16 +4,18 @@ import { AppDashboard } from "../../../static/js/pages/main/dashboard/app-dashbo
 require("../../../static/css/css-reset.css");
 export function dashboard_run(context) {
   const parsedContext = JSON.parse(context);
-  const { usertype, studentId, userLogin } = parsedContext;
+  const { usertype, userId, userLogin } = parsedContext;
 
   if (!userLogin) {
     window.location.href = "../user/";
   }
 
+  axios.defaults.headers["X-CSRFTOKEN"] = csrftoken;
+
   const main = document.querySelector("#main");
 
   const clNav = new NavManager(parsedContext);
-  const clManager = new AppDashboard({ usertype, studentId, userLogin });
+  const clManager = new AppDashboard({ usertype, studentId: userId, userLogin });
 
   main.append(clNav.getElement(), clManager.getElement());
 }
