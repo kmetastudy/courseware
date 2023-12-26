14b548b33aee28b6866302dea3e05e3170ba6ea8
muroa96 <thsgskwhd@gmail.com>
2023-12-26 16:26:04 +0900

    Fix: dashboard_view

--- _main/views.py
+++ _main/views.py
@@ -157,6 +157,8 @@ def dashboard_view(request):
 
     context = {'context': json.dumps(dashboard_context)}
 
+    return render(request, "_main/dashboard.html", context)
+
 
 @jwt_login_required
 def stats_view(request):
