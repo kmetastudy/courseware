d55866ea141127984be8d37c3b4a5b97214c135d
Bro-o <62311763+Bro-o@users.noreply.github.com>
2024-01-05 12:28:53 +0900

    코스 보여주기
    코스 리스트 페이징,
    break word, 자잘한 오류 수정

---
+++ static/js/pages/main/course/mtv-course.js
@@ -0,0 +1,52 @@
+import { CourseUnit } from "./mtu-course"
+import { PaginationUnit } from "./mtu-pagination"
+
+/*
+1. 코스 리스트 전체/필터링된
+2. 페이징
+*/
+const COUNT_PER_PAGE = 12
+
+export function CourseView(data) {
+  this.options = null
+  this.data = data
+  this.elThis = null
+  
+  this.currentPage = 1
+
+  this.init()
+  
+
+}
+
+CourseView.prototype.init = function() {
+  var $elCourseView = $(`<div>
+                          <div class="course-container"></div>
+                          
+                        </div>`)
+  let courseOptions = {currentPage:this.currentPage, countPerPage:COUNT_PER_PAGE}
+  var clCourseUnit = new CourseUnit(courseOptions, this.data)
+  $elCourseView.find('.course-container').append(clCourseUnit.elThis)
+
+  let pagenationOptions = {page:this.getTotalPageCount(), onClick:this.pageHandler.bind(this)}
+  var clPaginationUnit = new PaginationUnit(pagenationOptions)
+  $elCourseView.append(clPaginationUnit.elThis)
+  
+
+  this.elThis = $elCourseView
+
+}
+
+CourseView.prototype.getTotalPageCount = function() {
+  return Math.ceil(this.data.length / COUNT_PER_PAGE)
+}
+
+
+CourseView.prototype.pageHandler = function(el) {
+  console.log(el.target.innerText)
+  this.currentPage = el.target.innerText
+
+  let courseOptions = {currentPage:this.currentPage, countPerPage:COUNT_PER_PAGE}
+  var clCourseUnit = new CourseUnit(courseOptions, this.data)
+  $('.course-container').html(clCourseUnit.elThis)
+}
