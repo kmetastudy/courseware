e507125d61a058a7ed905505552ec611c92daa58
Bro-o <62311763+Bro-o@users.noreply.github.com>
2024-01-05 12:29:29 +0900

    네비게이션
    네브바 리스폰시브(버튼, 햄버거)

--- static/js/core/component/nav-manager.js
+++ static/js/core/component/nav-manager.js
@@ -16,7 +16,7 @@ export class NavManager {
   }
 
   prepareNavOptions() {
-    const logo = { image: "/static/assets/logo_courseware_white.png", onClick: () => (window.location.href = "/") };
+    const logo = { image: "/static/assets/logo_courseware_white.svg", onClick: () => (window.location.href = "/") };
     const menu = [
       { title: "초등", onClick: () => (window.location.href = "/courses/element/all"), color: "FFF500" },
       { title: "중등", onClick: () => (window.location.href = "/courses/middle/all"), color: "F6D5EA" },
@@ -30,35 +30,21 @@ export class NavManager {
     let login, logout;
     if (!this.userLogin) {
       login = [
-        {
-          text: "포인트 충전",
-          onClick: () => (window.location.href = "/point/charge/"),
-          icon: "Point.svg",
-          display: "hidden",
-        },
-        { text: "최근학습", onClick: () => (window.location.href = "#"), icon: "RecentStudy.svg", display: "hidden" },
-        { text: "장바구니", onClick: () => (window.location.href = "/cart/"), icon: "Cart.svg", display: "hidden" },
-        { text: "로그인", onClick: () => (window.location.href = "/user/"), icon: "Login.svg" },
-        { text: "회원가입", onClick: () => (window.location.href = "/user/"), type: "text", icon: "Signup.svg" },
-      ];
+        // { text: "포인트 충전", onClick: () => (window.location.href = "/point/charge/"), icon:"Point.svg", display:"close" },
+        // { text: "최근학습", onClick: () => (window.location.href = "#"), icon:"RecentStudy.svg", display:"close" },
+        // { text: "장바구니", onClick: () => (window.location.href = "/cart/"), icon:"Cart.svg", display:"close" },
+        { text: "로그인", onClick: () => (window.location.href = "/user/"), icon:"Login.svg" },
+        { text: "회원가입", onClick: () => (window.location.href = "/user/"), type: "text", icon:"Signup.svg" }
+      ]
     } else {
       logout = [
-        {
-          text: "포인트 충전",
-          onClick: () => (window.location.href = "/point/charge/"),
-          icon: "Point.svg",
-          display: "hidden",
-        },
-        { text: "최근학습", onClick: () => (window.location.href = "#"), icon: "RecentStudy.svg", display: "hidden" },
-        { text: "장바구니", onClick: () => (window.location.href = "/cart/"), icon: "Cart.svg", display: "hidden" },
-        { text: "마이페이지", onClick: () => (window.location.href = "/dashboard/"), icon: "Login.svg" },
-        {
-          text: "로그아웃",
-          onClick: () => (window.location.href = "/user/api/logout/"),
-          type: "text",
-          icon: "Signup.svg",
-        },
-      ];
+        { text: "포인트 충전", onClick: () => (window.location.href = "/point/charge/"), icon:"Point.svg" },
+        { text: "마이페이지", onClick: () => (window.location.href = "/dashboard/"), icon:"Dashboard.svg" },
+        { text: "최근학습", onClick: () => (window.location.href = "#"), icon:"RecentStudy.svg", display:"close" },
+        { text: "장바구니", onClick: () => (window.location.href = "/cart/"), icon:"Cart.svg", display:"close" },
+        
+        // { text: "로그아웃", onClick: () => (window.location.href = "/user/api/logout/"), type: "text", icon:"Signup.svg" }
+      ]
     }
 
     // dashboard = { text: "대시보드", onClick: () => (window.location.href = "/dashboard/") };
