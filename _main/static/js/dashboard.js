f95898cda8a3a6c1cccb33ef517f28dcccc3df4c
muroa96 <thsgskwhd@gmail.com>
2024-01-09 16:11:13 +0900

    Remove: dayjs duration plugin

--- _main/static/js/dashboard.js
+++ _main/static/js/dashboard.js
@@ -4,7 +4,6 @@ import { AppDashboard } from "../../../static/js/pages/main/dashboard/app-dashbo
 require("../../../static/css/css-reset.css");
 export function dashboard_run(context) {
   dayjs.extend(window.dayjs_plugin_relativeTime);
-  dayjs.extend(window.dayjs_plugin_duration);
   dayjs.locale("ko");
 
   const parsedContext = JSON.parse(context);
