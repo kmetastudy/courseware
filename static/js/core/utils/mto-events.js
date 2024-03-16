816ce8a2d635e2534dd9c9041f003c469d08f6a6
muroa96 <thsgskwhd@gmail.com>
2024-03-16 16:58:01 +0900

    Style: Code format changed

--- static/js/core/utils/mto-events.js
+++ static/js/core/utils/mto-events.js
@@ -6,37 +6,35 @@
 // https://gist.github.com/learncodeacademy/777349747d8382bfb722
 
 export var mtoEvents = {
-    events: {},
-    on: function (eventName, fn) {
-        // console.log('eventName : ', eventName);
-        // console.log('eventFn : ', fn);
-        
-        this.events[eventName] = this.events[eventName] || [];
-        this.events[eventName].push(fn);
-    },
-    off: function(eventName, fn) {
-        if (this.events[eventName]) {
-            for (var i = 0; i < this.events[eventName].length; i++) {
-                if (this.events[eventName][i] === fn) {
-                    this.events[eventName].splice(i, 1);
-                    break;
-                }
-            };
-        }
-    },
-    emit: function (eventName, data) {
-        // console.log('emit : ', eventName);
-        var retval;
-        if (this.events[eventName]) {
-            // console.log(this.events[eventName]);
-            this.events[eventName].forEach(function(fn) {
-                var ret = fn(data);
-                if(ret)
-                    retval = ret;
-            });   
-        }
+  events: {},
+  on: function (eventName, fn) {
+    // console.log('eventName : ', eventName);
+    // console.log('eventFn : ', fn);
 
-        if(retval)
-            return retval;
+    this.events[eventName] = this.events[eventName] || [];
+    this.events[eventName].push(fn);
+  },
+  off: function (eventName, fn) {
+    if (this.events[eventName]) {
+      for (var i = 0; i < this.events[eventName].length; i++) {
+        if (this.events[eventName][i] === fn) {
+          this.events[eventName].splice(i, 1);
+          break;
+        }
+      }
     }
+  },
+  emit: function (eventName, data) {
+    // console.log('emit : ', eventName);
+    var retval;
+    if (this.events[eventName]) {
+      // console.log(this.events[eventName]);
+      this.events[eventName].forEach(function (fn) {
+        var ret = fn(data);
+        if (ret) retval = ret;
+      });
+    }
+
+    if (retval) return retval;
+  },
 };
