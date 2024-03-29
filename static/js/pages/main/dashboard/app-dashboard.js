906b3d7dd9e1b3c5dda94d66bcae2bf1cb618877
muroa96 <thsgskwhd@gmail.com>
2024-03-29 17:55:17 +0900

    Feat: Make teacher go to dashboard after login

--- static/js/pages/main/dashboard/app-dashboard.js
+++ static/js/pages/main/dashboard/app-dashboard.js
@@ -48,33 +48,36 @@ export class AppDashboard {
     this.clSide = new mtmSideMenu({ item: this.sideItems });
     this.elSide = this.clSide.getElement();
 
-    this.elDashboard = createElement("div", { className: "dashboard-wrapper" });
+    this.elBody = createElement("div", { className: "dashboard-wrapper" });
 
     const { userType, userId: studentId, userLogin } = this.config;
-    this.clDashboardManager = new MtmDashboardManager({ userType, studentId, userLogin });
-    this.elDashboardManager = this.clDashboardManager.getElement();
-    this.elDashboard.append(this.elDashboardManager);
 
     switch (this.config.userType) {
       case TYPE_MEMBER.TEACHER:
         this.clClassManager = new TeacherClassManager({ userId: this.config.userId });
         this.elClassManager = this.clClassManager.getElement();
-        this.elDashboard.append(this.elClassManager);
+        this.elBody.append(this.elClassManager);
 
         this.clInactiveClassManager = new InactiveClassManager({ userId: this.config.userId });
         this.elInactiveClassManager = this.clInactiveClassManager.getElement();
-        this.elDashboard.append(this.elInactiveClassManager);
+        this.elBody.append(this.elInactiveClassManager);
+
+        this.handleSideClick("class");
         break;
       default:
+        this.clDashboardManager = new MtmDashboardManager({ userType, studentId, userLogin });
+        this.elDashboardManager = this.clDashboardManager.getElement();
+        this.elBody.append(this.elDashboardManager);
+
         this.clClassManager = new StudentClassManager({ userId: this.config.userId });
         this.elClassManager = this.clClassManager.getElement();
-        this.elDashboard.append(this.elClassManager);
+        this.elBody.append(this.elClassManager);
+
+        this.handleSideClick("dashboard");
         break;
     }
 
-    this.handleSideClick("dashboard");
-
-    this.elThis.append(this.elSide, this.elDashboard);
+    this.elThis.append(this.elSide, this.elBody);
   }
 
   createTeacherSideItems() {
@@ -82,8 +85,8 @@ export class AppDashboard {
       {
         title: "클래스",
         children: [
-          { title: "내 클래스", onClick: this.handleSideClick.bind(this, "class") },
-          { title: "종료된 클래스", onClick: this.handleSideClick.bind(this, "inactiveClass") },
+          { title: "내 클래스", onClick: this.handleSideClick.bind(this, "class"), key: "class" },
+          { title: "종료된 클래스", onClick: this.handleSideClick.bind(this, "inactiveClass"), key: "inactiveClass" },
         ],
       },
       // { title: "설정", children: [{ title: "계정 정보" }, { title: "알림 설정" }] },
@@ -94,28 +97,31 @@ export class AppDashboard {
 
   createDefaultSideItems() {
     const sideItems = [
-      { title: "대시보드" },
-      { title: "프로필", onClick: () => (window.location.href = "/"), icon: "user" },
-      { title: "통계", onClick: () => (window.location.href = "../stats/"), icon: "barChart" },
+      { title: "대시보드", key: "dashboard" },
+      { title: "프로필", onClick: () => (window.location.href = "/"), icon: "user", key: "profile" },
+      { title: "통계", onClick: () => (window.location.href = "../stats/"), icon: "barChart", key: "stats" },
       {
         title: "클래스",
-        children: [{ title: "내 클래스", onClick: this.handleSideClick.bind(this, "class") }, { title: "수강전 문의" }],
+        children: [{ title: "내 클래스", onClick: this.handleSideClick.bind(this, "class"), key: "class" }],
       },
       {
         title: "학습 관리",
-        children: [{ title: "내 학습", onClick: () => (window.location.href = "../mycourse/") }],
+        children: [{ title: "내 학습", onClick: () => (window.location.href = "../mycourse/"), key: "mycourse" }],
       },
       {
         title: "수강신청 관리",
         children: [
-          { title: "수강바구니", onClick: () => (window.location.href = "../cart/") },
-          { title: "좋아요" },
-          { title: "쿠폰함" },
-          { title: "포인트" },
-          { title: "구매내역", onClick: () => (window.location.href = "../orders/") },
+          { title: "수강바구니", onClick: () => (window.location.href = "../cart/"), key: "cart" },
+          { title: "구매내역", onClick: () => (window.location.href = "../orders/"), key: "orders" },
         ],
       },
-      { title: "설정", children: [{ title: "계정 정보" }, { title: "알림 설정" }] },
+      {
+        title: "설정",
+        children: [
+          { title: "계정 정보", key: "accountSetting" },
+          { title: "알림 설정", key: "notificationSetting" },
+        ],
+      },
     ];
 
     return sideItems;
@@ -131,22 +137,22 @@ export class AppDashboard {
 
     switch (key) {
       case "dashboard":
-        this.elDashboardManager.classList.remove("hidden");
-        this.currentContent = this.elDashboardManager;
+        this.elDashboardManager?.classList.remove("hidden");
+        this.currentContent = this?.elDashboardManager;
         break;
       case "class":
         this.elClassManager.classList.remove("hidden");
         this.currentContent = this.elClassManager;
         break;
-
       case "inactiveClass":
         this.elInactiveClassManager?.classList.remove("hidden");
         this.currentContent = this?.elInactiveClassManager;
         break;
-
       default:
         break;
     }
+
+    this.clSide.activate(key);
   }
 
   getElement() {
