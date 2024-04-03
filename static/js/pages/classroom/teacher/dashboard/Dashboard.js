6d8ba7c25ba50694afeae6ec1c09156aef20d9c1
Bro-o <62311763+Bro-o@users.noreply.github.com>
2024-04-03 13:45:23 +0900

    선생님 임시 대시보드
    테스트로 만든거 임시로 붙임

--- static/js/pages/classroom/teacher/dashboard/Dashboard.js
+++ static/js/pages/classroom/teacher/dashboard/Dashboard.js
@@ -2,7 +2,7 @@ import elem from "../../../../core/utils/elem/elem";
 
 import { store } from "../Store";
 
-import Stat from "./stat/Stat";
+import Stat from "../stat/Stat";
 
 export class Dashboard {
   constructor() {
--- static/js/pages/classroom/teacher/stat/layout/organisms/LessonStudent.js
+++ static/js/pages/classroom/teacher/stat/layout/organisms/LessonStudent.js
@@ -1,9 +1,9 @@
 const LessonStudent = props => {
     return `
         <li class="flex justify-between items-center p-2 gap-4">
-            <div class="flex items-center">
-                <svg data-src="https://unpkg.com/heroicons/20/solid/user.svg" class="h-5 w-5"></svg>
-                <p class="m-0">${props.name}</p>
+            <div class="flex flex-none items-center">
+                
+                <p class="m-0 text-sm">${props.name}</p>
             </div>
             <div class="">
                 <div class="flex justify-between items-center">
@@ -12,8 +12,8 @@ const LessonStudent = props => {
                 </div>
                 <progress class="progress progress-success w-30" value="${props.progress}" min="0" max="100"></progress>
             </div>
-            <div class="text-center">
-                <p class="text-sm m-0">맞춘 문제 수</p>
+            <div class="flex flex-col flex-none text-center">
+                <p class="text-xs m-0">맞춘 문제 수</p>
                 <p class="text-sm m-0">${props.point}/${props.numOfQuestion}</p>
             </div>
             <button class="btn btn-sm btn-disabled">확인하기</button>
