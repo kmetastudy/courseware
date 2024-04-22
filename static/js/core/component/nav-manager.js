c10d4d156e372f842e0955faddfd2fa43b8b235f
Bro-o <62311763+Bro-o@users.noreply.github.com>
2024-04-22 19:56:49 +0900

    학교페이지
    군산초 추가

--- static/js/core/component/nav-manager.js
+++ static/js/core/component/nav-manager.js
@@ -34,14 +34,14 @@ export class NavManager {
         // { text: "포인트 충전", onClick: () => (window.location.href = "/point/charge/"), icon:"Point.svg", display:"close" },
         // { text: "최근학습", onClick: () => (window.location.href = "#"), icon:"RecentStudy.svg", display:"close" },
         // { text: "장바구니", onClick: () => (window.location.href = "/cart/"), icon:"Cart.svg", display:"close" },
-        { text: "문의하기", onClick: () => (window.location.href = "/contact/"), icon:"" },
+        { text: "문의하기", onClick: () => (window.location.href = "/contact/"), icon:"Dashboard.svg" },
         { text: "로그인", onClick: () => (window.location.href = "/user/"), icon:"Login.svg" },
         { text: "회원가입", onClick: () => (window.location.href = "/user/"), type: "text", icon:"Signup.svg" }
       ]
     } else {
       logout = [
         // { text: "포인트충전", onClick: () => (window.location.href = "/point/charge/"), icon:"Point.svg" },
-        { text: "문의하기", onClick: () => (window.location.href = "/contact/"), icon:"" },
+        { text: "문의하기", onClick: () => (window.location.href = "/contact/"), icon:"Dashboard.svg" },
         { text: "마이페이지", onClick: () => (window.location.href = "/dashboard/"), icon:"Dashboard.svg" },
         { text: "최근학습", onClick: () => (window.location.href = "#"), icon:"RecentStudy.svg" },
         // { text: "장바구니", onClick: () => (window.location.href = "/cart/"), icon:"Cart.svg", display:"close" },
