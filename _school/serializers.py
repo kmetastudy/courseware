b052c701051e77552ae7be46888f25d3de00553f
Bro-o <62311763+Bro-o@users.noreply.github.com>
2024-05-29 16:44:39 +0900

    fix: 학교 보유 코스 썸네일, 타이틀 변경 반영

--- _school/serializers.py
+++ _school/serializers.py
@@ -9,10 +9,35 @@ from _school.models import mSchool, mSchoolCourse, mSchoolNotice, mSchoolSection
 class CourseSerializer(serializers.ModelSerializer):
 
     course = CourseDetailSerializer(read_only=True)
+    modified_course_title = serializers.CharField(required=False)
+    modified_thumnail = serializers.CharField(required=False)
 
     class Meta:
         model = mSchoolCourse
-        fields = ["course"]
+        fields = ["course", "modified_course_title", "modified_thumnail"]
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        # data = {}
+
+        # if instance.modified_course_title:
+        #     data["courseTitle"] = instance.modified_course_title
+        # else:
+        #     data["courseTitle"] = instance.course.courseTitle
+
+        # if instance.modified_thumnail:
+        #     data["thumnail"] = instance.modified_thumnail
+        # else:
+        #     data["thumnail"] = instance.course.thumnail
+
+        data["course"]["courseTitle"] = (
+            instance.modified_course_title or instance.course.courseTitle
+        )
+        data["course"]["thumnail"] = (
+            instance.modified_thumnail or instance.course.thumnail
+        )
+
+        return data
 
 
 class SectionSerializer(serializers.ModelSerializer):
