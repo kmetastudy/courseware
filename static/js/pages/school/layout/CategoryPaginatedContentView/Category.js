5eed1a7e166d1e9b07f8ab30a63b9b67fbc169f8
Bro-o <62311763+Bro-o@users.noreply.github.com>
2024-06-26 20:45:41 +0900

    랜딩 테스트 수정

--- static/js/pages/school/layout/CategoryPaginatedContentView/Category.js
+++ static/js/pages/school/layout/CategoryPaginatedContentView/Category.js
@@ -1,7 +1,7 @@
 const Category = (sections) => {
 
     return `
-        <div data-component="category" class="categoryMobile w-fit text-[#474747] font-normal" style="margin-top:20px;margin-bottom: 40px;">
+        <div data-component="category" class="categoryMobile w-fit text-[#474747] font-normal text-center break-keep" style="margin-top:20px;margin-bottom: 40px;">
             <div class="category-click cursor-pointer text-[#3db051] font-bold" data-seq="0">전체보기</div>
             ${sections.map(({title}, index) => {
                 return `<div class="category-click cursor-pointer" data-seq="${index+1}">${title}</div>`
