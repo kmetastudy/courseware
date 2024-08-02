ebd81502d8bcf27b98f80352afced8d388d408a6
Bro-o <62311763+Bro-o@users.noreply.github.com>
2024-08-02 19:33:06 +0900

    배너 변경

--- _cm/models.py
+++ _cm/models.py
@@ -139,3 +139,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Banner(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="banners/")
+    image_mobile = models.ImageField(upload_to="banners/")
+    link = models.URLField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    seq = models.IntegerField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
