ec4de676aaaa4ba88f430d05b2dcc05dacfc9969
Bro-o <62311763+Bro-o@users.noreply.github.com>
2024-08-20 15:39:40 +0900

    번들 날짜 변경 및 학교페이지 수정

--- webpack.config.js
+++ webpack.config.js
@@ -104,14 +104,14 @@ var configSTSchool = Object.assign({}, config, {
   output: {
     path: path.resolve(__dirname, "static/_bundle/st_school"),
     library: "stSchoollibrary",
-    filename: "st-school-bundle-20240802.js",
+    filename: "st-school-bundle-20240820.js",
     libraryTarget: "var",
     clean: true,
     // assetModuleFilename: '[name][ext]',
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "st-school-bundle-20240802.css",
+      filename: "st-school-bundle-20240820.css",
     }),
   ],
 });
@@ -125,14 +125,14 @@ var configSchool = Object.assign({}, config, {
   output: {
     path: path.resolve(__dirname, "static/_bundle/school"),
     library: "schoollibrary",
-    filename: "school-bundle-20240802.js",
+    filename: "school-bundle-20240820.js",
     libraryTarget: "var",
     clean: true,
     // assetModuleFilename: '[name][ext]',
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "school-bundle-20240802.css",
+      filename: "school-bundle-20240820.css",
     }),
   ],
   module: tailwindModuleConfig,
